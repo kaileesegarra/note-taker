a2b54ad9b7868a8e99d220096cd29b453f3ad826
kaileesegarra <158116690+kaileesegarra@users.noreply.github.com>
2024-07-15 14:54:25 -0400

    server.js updates

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const path = require('path');
 
@@ -5,3 +6,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
