49b7ec227eb7d4642654f9eaec57ebbb312b2f9a
kaileesegarra <158116690+kaileesegarra@users.noreply.github.com>
2024-07-26 07:40:01 -0400

    routes for note operations

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = (app) => {
+  const dbPath = path.join(__dirname, '../db/db.json');
+
+  app.get('/api/notes', (req, res) => {
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        res.json(JSON.parse(data));
+      }
+    });
+  });
+
+  app.post('/api/notes', (req, res) => {
+    const newNote = { ...req.body, id: uuidv4() };
+
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+
+        fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+          } else {
+            res.json(newNote);
+          }
+        });
+      }
+    });
+  });
+
+  app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        const notes = JSON.parse(data);
+        const updatedNotes = notes.filter(note => note.id !== noteId);
+
+        fs.writeFile(dbPath, JSON.stringify(updatedNotes), (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+          } else {
+            res.json({ id
